cbeb801f6c5707e41cf8a75cdec4aab95f51b6aa
Erika Moraes Vaz Nascimento <erikamoraesv@gmail.com>
2020-06-23 21:40:12 -0300

    first version
    first version

---
+++ frontEnd/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Spinner from "./components/Spinner";
+import Candidates from "./components/Candidates"
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      candidates: [],
+      previousVotes: [],
+      previousPercentage: [],
+    };
+
+    this.interval = null;
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      fetch("http://localhost:8080/votes")
+        .then((res) => {
+          return res.json();
+        })
+        .then((json) => {
+
+          const previousVotes = this.state.candidates.map(({id, votes})=>{
+            return {id, votes};
+          });
+
+          const previousPercentage = this.state.candidates.map(({id, percentage})=>{
+            return {id, percentage};
+          });
+
+          this.setState({
+            candidates: json.candidates,
+            previousVotes,
+            previousPercentage,
+          });
+        });
+    }, 1000);
+  }
+
+  render() {
+    const { candidates, previousVotes, previousPercentage } = this.state;
+
+    if (candidates.length === 0) {
+      console.log("oii");
+      return <Spinner description="carregando..." />;
+    }
+    return (
+      <div className="container">
+        <Header>Votação</Header>
+        <Candidates previousVotes={previousVotes} previousPercentages={previousPercentage} candidates={candidates} />
+      </div>
+    );
+  }
+}
